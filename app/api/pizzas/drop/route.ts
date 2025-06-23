686130d6a5764600ec0a22403a19a000ac7f4be9
Michael Leners <micleners@gmail.com>
2025-06-23 13:07:43 -0500

    Remove pizzas repo and endpoints

--- app/api/pizzas/drop/route.ts
+++
@@ -1,8 +0,0 @@
-import { dropAssets } from "@/app/db/assets/repository";
-
-export async function GET() {
-  const result = await dropAssets();
-  return new Response(JSON.stringify(result), {
-    headers: { 'Content-Type': 'application/json' },
-  });
-} 
--- app/api/pizzas/route.ts
+++
@@ -1,8 +0,0 @@
-import { getAssets } from "@/app/db/assets/repository";
-
-export async function GET() {
-  const assets = await getAssets();
-  return new Response(JSON.stringify(assets), {
-    headers: { 'Content-Type': 'application/json' },
-  });
-} 
